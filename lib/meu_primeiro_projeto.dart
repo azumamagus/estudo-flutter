2e5869d7895bcb77ef43a44f6c133708ccb6c906
Bruno Felipe <bruno.dc.felipe@gmail.com>
2023-08-01 07:07:24 -0300

    Iniciando Projeto

---
+++ lib/meu_primeiro_projeto.dart
@@ -0,0 +1,44 @@
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Column(
+// mainAxisAlignment: MainAxisAlignment.start,
+// children: [
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// children: [
+// Container(color: Colors.red, width: 50, height: 50),
+// Container(color: Colors.blue, width: 50, height: 50),
+// Container(color: Colors.yellow, width: 50, height: 50)
+// ],
+// ),
+// ],
+// ),
+// Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+// children: [
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// children: [
+// Container(color: Colors.red, width: 50, height: 50),
+// Container(color: Colors.blue, width: 50, height: 50),
+// Container(color: Colors.yellow, width: 50, height: 50)
+// ],
+// ),
+// ],
+// ),
+// Column(
+// mainAxisAlignment: MainAxisAlignment.end,
+// children: [
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// children: [
+// Container(color: Colors.red, width: 50, height: 50),
+// Container(color: Colors.blue, width: 50, height: 50),
+// Container(color: Colors.yellow, width: 50, height: 50)
+// ],
+// ),
+// ],
+// ),
+// ],
+// )
